83f4a33e8e3fb9226fe015b9c004cee37b143860
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2025-10-10 17:54:27 +0530

    Update geminii.js

--- src/lib/geminii.js
+++ src/lib/geminii.js
@@ -5,7 +5,7 @@ const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEYYY);
 
 export async function generateWithGemini({ sectionType, userInput, context }) {
   try {
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash-exp" });
 
     const prompt = `
       Generate resume content for the section: ${sectionType}.
